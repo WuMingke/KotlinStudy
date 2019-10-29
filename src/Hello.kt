eb2bba69fca4bcbbff0e8ae35deb77bf1ae5181b
wumingke <766190553@qq.com>
2019-10-29 14:41:24 +0800

    基本语法

--- src/Hello.kt
+++ src/Hello.kt
@@ -5,6 +5,57 @@ import obj.Person
  */
 
 fun main(args: Array<String>) {
-    println("123")
-    Person("qinglie").printName()
+
+//    println("123")
+//    Person("qinglie").printName()
+
+//    val quantity = 5
+//    val price: Double = 20.3
+//    val name: String = "rice"
+//
+//    println("price:$price")
+//    println("quantity:$quantity")
+//    println("product:$name  total:${quantity * price}")
+
+
+//    fun cases(obj: Any) {
+//        when (obj) {
+//            1 -> println("1")
+//            "hello" -> println("String hello")
+//            is Long -> println("is Long")
+//            !is Long -> println("is not Long")
+//            else -> println("default")
+//        }
+//    }
+//
+//    cases(1)
+//    println()
+//    cases(2L)
+//    println()
+//    cases("")
+
+//    fun say(str: String): String {
+//        return str
+//    }
+//
+//    fun say1(str: String) = str
+//
+//    println(say1("jack"))
+//
+//
+//    fun say2(str: String = "bob") = str
+//
+//    println(say2("lucy"))
+
+
+//    fun hasEmpty(vararg str: String?): Boolean {
+//        for (strItem in str) {
+//            if ("" == strItem || null == strItem) return true
+//        }
+//        return false
+//    }
+//
+
+
+
 }
