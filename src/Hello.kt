555edaf4a42a22e655b0d15330078bdf1fe77787
wumingke <766190553@qq.com>
2019-10-29 12:01:12 +0800

    Initial commit

---
+++ src/Hello.kt
@@ -0,0 +1,10 @@
+import obj.Person
+
+/**
+ * Created by qingLie on 2019/10/29.
+ */
+
+fun main(args: Array<String>) {
+    println("123")
+    Person("qinglie").printName()
+}
---
+++ src/obj/Person.kt
@@ -0,0 +1,11 @@
+package obj
+
+/**
+ * Created by qinglie on 2019/10/29.
+ */
+
+class Person(val name: String) {
+    fun printName() {
+        println(name)
+    }
+}
